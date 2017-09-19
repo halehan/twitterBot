d4c375a226e1659502318e64097c8f5eddc1135d
Thomas D Williams <dwilliams@halehan.com>
2017-09-19 16:35:58 -0500

    Added js files

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  consumer_key: 'GANeK5giuFWOopUJRysTbsVip',
+  consumer_secret: 'ATA4fDi7TBT6ETW1KdXUKHNkVrAsy9hCjTrscTQMq2eYmgw9vK',
+  access_token_key: '13948572-fAA9NzZ3XTja3NoCotMbVchlFQZYYLlG3lnwfKhpR',
+  access_token_secret: 'StGbXiLND4JbLoip8hxU78VNa9jBHF0qg9Lg01Ke5Tddf'
+}
---
+++ listTweets.js
@@ -0,0 +1,27 @@
+// app.js
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+  q: '@getongab',
+  count: 200,
+  lang: 'en'
+}
+
+// Initiate your search using the above paramaters
+T.get('search/tweets', params, function(err, data, response) {
+  // If there is no error, proceed
+  if(!err){
+    // Loop through the returned tweets
+    for(let i = 0; i < data.statuses.length; i++){
+      // Get the tweet Id from the returned data
+      let id = { id: data.statuses[i].id_str }
+      console.log(id);
+    
+    }
+  } else {
+    console.log(err);
+  }
+})
